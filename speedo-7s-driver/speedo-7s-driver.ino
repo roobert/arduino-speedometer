f10758e9a2a5804e6c1c82c6a2b26a9a9d92f30b
Rob Wilson <roobert@gmail.com>
2018-08-22 18:04:21 +0100

    placeholder comment

--- speedo-7s-driver/speedo-7s-driver.ino
+++ speedo-7s-driver/speedo-7s-driver.ino
@@ -1,13 +1,1 @@
-// LED speed-o-meter
-//
-// inspiration
-// http://www.instructables.com/id/Bike-Speedometer/
-//
-// hs320401k-24 3 digit
-// http://www.marthel.pl/katalog/Betlux/BL-T52A-31.pdf
-//
-// 7447 7SD decoder
-// https://www.electroschematics.com/wp-content/uploads/2013/01/7447-datasheet.pdf
-// works with anode display
-
-
+// placeholder file to allow use of arduino ide to upload sketches
