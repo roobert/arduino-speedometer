2e15a35dc8433c5cd5f1f780ab5a87545c9d19a8
Rob Wilson <roobert@gmail.com>
2018-08-26 17:33:34 +0100

    correct pins, add diagram

--- speedo-7s-driver/_pins_attiny.ino
+++ speedo-7s-driver/_pins_attiny.ino
@@ -1,6 +1,16 @@
 /*
-// Hall Effect Sensor
-const int pinHES = 6;
+
+  ATTINY44 / ATTINY84
+
+     VCC -###- GND
+  pin 10 -###- pin 0
+   pin 9 -###- pin 1
+     RST -###- pin 2
+   pin 8 -###- pin 3
+   pin 7 -###- pin 4
+   pin 6 -###- pin 5
+
+*/
 
 // decoder pins
 const int pinDecA = 0;
@@ -10,8 +20,11 @@ const int pinDecD = 4;
 const int decoderPins[] = { pinDecA, pinDecB, pinDecC, pinDecD };
 
 // common pins control digit selection; LOW is on
-const int pinC1 = 6;
-const int pinC2 = 7;
-const int pinC3 = 8;
+const int pinC1 = 7;
+const int pinC2 = 8;
+const int pinC3 = 9;
 const int controlPins[] = { pinC1, pinC2, pinC3 };
-*/
+
+// Hall Effect Sensor
+const int pinHES = 5;
+
