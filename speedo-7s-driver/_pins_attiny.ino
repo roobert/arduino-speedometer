074f502f72165046e326b608feee439655f1cdb7
Rob Wilson <roobert@gmail.com>
2018-08-26 17:43:56 +0100

    correct pins

--- speedo-7s-driver/_pins_attiny.ino
+++ speedo-7s-driver/_pins_attiny.ino
@@ -20,11 +20,10 @@ const int pinDecD = 4;
 const int decoderPins[] = { pinDecA, pinDecB, pinDecC, pinDecD };
 
 // common pins control digit selection; LOW is on
-const int pinC1 = 7;
-const int pinC2 = 8;
-const int pinC3 = 9;
+const int pinC1 = 6;
+const int pinC2 = 7;
+const int pinC3 = 8;
 const int controlPins[] = { pinC1, pinC2, pinC3 };
 
 // Hall Effect Sensor
 const int pinHES = 5;
-
